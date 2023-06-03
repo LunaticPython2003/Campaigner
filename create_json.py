e0fa7509dffca2285d97c326a35a6807f6ea1017
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-06-03 16:22:48 +0530

    Added some other parameters

--- create_json.py
+++ create_json.py
@@ -1,16 +1,21 @@
 import requests
 
 url = 'http://127.0.0.1:5000/channels'
-jwt_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4NTc3MzExNSwianRpIjoiNmNlY2UyYjgtOWU5My00ZWZlLWJiMmUtNjcxY2NhYzJmZTZmIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6WzI3MzcyXSwibmJmIjoxNjg1NzczMTE1LCJleHAiOjE2ODU3NzQwMTV9.Yf7sSK1IS61D5XmQm3LHJx3eIlJ03hMrgX1ml1fBw-Q'
-channels = ["Whatsapp", "SMS", "RCS"]
+jwt_token = 'your_token'
+channels = {
+    "WhatsApp": {"Phone": 4539405830, "Message":"Something"},
+    "SMS": {"Phone": 3578923952, "Message": "Something"}
+}
+
+chunks = 50
 
 headers = {
     'Authorization': f'Bearer {jwt_token}',
     'Content-Type': 'application/json'
 }
 data = {
-    'jwt': jwt_token,
-    'channels': channels
+    'channels': channels,
+    'chunks': chunks
 }
 
 response = requests.post(url, json=data, headers=headers)
