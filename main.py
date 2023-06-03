d5047ee5b8e0b06557c447a4389091ce6060717d
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-06-03 16:23:27 +0530

    Added the basic logic

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ USER = os.getenv('USER')
 # Configure the Flask App
 app = Flask(__name__)
 jwt = JWTManager(app)
-app.config['JWT_TOKEN_LOCATION'] = ['headers', 'query_string']
+app.config['JWT_TOKEN_LOCATION'] = ['headers']
 app.config["JWT_SECRET_KEY"] = secret_key
-app.config['CACHE_TYPE'] = 'simple'
+# app.config['CACHE_TYPE'] = 'simple'
 app.debug = True
 
 
@@ -40,28 +40,59 @@ def validate():
         access_token = create_access_token(identity=userid)
         return jsonify({ "token": access_token, "user_id": userid })
     
+
+# @app.route('/settings')
+# def settings():
+
     
 class ChannelsView(MethodView):
     threads = 10
+    priority = {"SMS": 1, "WhatsApp": 2, "email": 3, "RCS": 4}
+    chunks = float('inf')
+    channel_methods = []
 
     @jwt_required()
     def post(self):
-        json_data = request.get_json()
-        jwt_token = json_data.get('jwt')
+        auth_header = request.headers.get('Authorization')
+        jwt_token = auth_header.split("Bearer ")[1] if auth_header and auth_header.startswith("Bearer ") else None
         current_user_id = str(get_jwt_identity()[0])
 
+        json_data = request.get_json()
         db_cursor.execute(f"SELECT username FROM customers WHERE userid={current_user_id}")
         user = db_cursor.fetchone()[0]
         channels = json_data.get('channels')
-        print(channels)
+        channel_methods = [x for x in channels.keys()]
+        self.channel_methods = sorted(channel_methods, key=lambda x: self.priority[x])
+        chunks = json_data.get('chunks')
+        self.chunks = chunks
+        return "Hello World"
         if user is not None:
-            return channels
-            executor = ThreadPoolExecutor(max_workers = self.threads)
-            for task in channels:
-                executor.submit(self.task)
+            # executor = ThreadPoolExecutor(max_workers = self.threads)
+            # for task in channels:
+            #     executor.submit(self.task, methods[task])
+            for process in channel_methods:
+                if hasattr(self, process):
+                    func = getattr(self, process)
+                    return func(channels[process])
             
         else:
             return jsonify({'msg': 'Access forbidden'}), 403
+        
+    def whatsapp(self, method):
+        print(method)
+        return jsonify(method)
+
+    def SMS(self, method):
+        print(method)
+        return jsonify(method)
+
+    def RCS(self, method):
+        print(method)
+        return jsonify(method)
+
+    def email(self, method):
+        print(method)
+        return jsonify(method)
     
 app.add_url_rule('/channels', view_func=ChannelsView.as_view('channels'))
 
