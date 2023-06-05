ad21e27a901759b08dca722906cb65d3f9eeeb37
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-06-05 12:46:30 +0530

    Added /settings route

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
 from flask.views import MethodView
-from concurrent.futures import ThreadPoolExecutor
 import mysql.connector as ms
 import os
 from dotenv import load_dotenv
+import threading
 
 load_dotenv()
 
@@ -40,69 +40,76 @@ def validate():
         return jsonify({ "token": access_token, "user_id": userid })
     
 
-@app.route('/settings')
-def settings():
-    pass
-
     
 class ChannelsView(MethodView):
-    # threads = 10
-    # priority = {"SMS": 1, "WhatsApp": 2, "email": 3, "RCS": 4}
-    # chunks = float('inf')
-    # channel_methods = []
-
-    @jwt_required
-    def settings_post(self):
-        auth_header = request.headers.get('Authorization')
-        jwt_token = auth_header.split("Bearer ")[1] if auth_header and auth_header.startswith("Bearer ") else None
-        current_user_id = str(get_jwt_identity()[0])
-
-        json_data = request.get_json()
-        
+    threads = 10
+    priority = {"Sms": 1, "Whatsapp": 2, "Email": 3, "Rcs": 4}
+    chunks = float('inf')
+    channel_methods = []
 
     @jwt_required()
     def post(self):
         auth_header = request.headers.get('Authorization')
         jwt_token = auth_header.split("Bearer ")[1] if auth_header and auth_header.startswith("Bearer ") else None
         current_user_id = str(get_jwt_identity()[0])
-
         json_data = request.get_json()
-        db_cursor.execute(f"SELECT username FROM customers WHERE userid={current_user_id}")
-        user = db_cursor.fetchone()[0]
-        channels = json_data.get('channels')
-        channel_methods = [x for x in channels.keys()]
-        self.channel_methods = sorted(channel_methods, key=lambda x: self.priority[x])
-        chunks = json_data.get('chunks')
-        self.chunks = chunks
-        if user is not None:
-            # executor = ThreadPoolExecutor(max_workers = self.threads)
-            # for task in channels:
-            #     executor.submit(self.task, methods[task])
-            for process in channel_methods:
-                if hasattr(self, process):
-                    func = getattr(self, process)
-                    return func(channels[process])
-            
+        if request.path == '/settings':
+            return self.handle_settings_post(current_user_id, json_data)
         else:
-            return jsonify({'msg': 'Access forbidden'}), 403
+            db_cursor.execute(f"SELECT username FROM customers WHERE userid={current_user_id}")
+            user = db_cursor.fetchone()[0]
+            channels = json_data.get('channels')
+            self.channel_methods = sorted(channels.keys(), key=lambda x: self.priority[x])
+            chunks = json_data.get('chunks')
+            self.chunks = chunks
+            if user is not None:
+                threads = []
+                for process in self.channel_methods:
+                    if hasattr(self, process):
+                        func = getattr(self, process)
+                        thread = threading.Thread(target=func, args=(channels[process],))  # Capitalize key
+                        threads.append(thread)
+                        thread.start()
+
+                for thread in threads:
+                    thread.join()
+
+                return jsonify({'msg': 'Processing completed'})
+
+            else:
+                return jsonify({'msg': 'Access forbidden'}), 403
+            
+    def handle_settings_post(self, current_user_id, json_data):
+        threads = json_data.get('threads')
+        priority = json_data.get('priority')
+        chunks = json_data.get('chunks')
+        try:
+            db_cursor.execute(f'INSERT INTO settings (UserId, threads, priority, chunks) VALUES ({current_user_id}, {threads}, "{priority}", {chunks})')
+            mydb.commit()
+            return jsonify(message='Added record successfully')
+        except ms.errors.IntegrityError as err:
+            return jsonify(message=str(err))
+
+    @staticmethod
+    def Whatsapp(method):
+        print(method)
+
+    @staticmethod
+    def Sms(method):
+        print(method)
+
+    @staticmethod
+    def Rcs(method):
+        print(method)
+
+    @staticmethod
+    def Email(method):
+        print(method)
         
-    def whatsapp(self, method):
-        print(method)
-        return jsonify(method)
-
-    def SMS(self, method):
-        print(method)
-        return jsonify(method)
-
-    def RCS(self, method):
-        print(method)
-        return jsonify(method)
-
-    def email(self, method):
-        print(method)
-        return jsonify(method)
     
-app.add_url_rule('/channels', view_func=ChannelsView.as_view('channels'))
+app.add_url_rule('/channels', view_func=ChannelsView.as_view('channels'), methods=['POST'])
+app.add_url_rule('/settings', view_func=ChannelsView.as_view('post'), methods=['POST'])
+
 
 if __name__=="__main__":
     app.run(debug=True)
