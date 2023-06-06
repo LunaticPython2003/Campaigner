653d30b447d7e1b22eb9ec7f92c940f1f6e8ec95
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-06-06 16:49:05 +0530

    Added Scheduling

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Readme to track progress
 - [x] Create the two routes
 - [x] Fetch details of a userid from settings table
 - [x] <b> Check for Race Conditions </b>
+- [x] Create a table campaigner
+- [x] If data for priority exists in settings, update from 
+- [x] Implemented scheduling
 - [ ] Implement Failsafe
--- campaigner/main.py
+++ campaigner/main.py
@@ -5,7 +5,7 @@ import mysql.connector as ms
 import os
 from concurrent.futures import ThreadPoolExecutor, wait
 from dotenv import load_dotenv
-
+import time, datetime
 from threading import Lock
 import json
 
@@ -47,7 +47,8 @@ class ChannelsView(MethodView):
     threads = 0
     priority = dict()
     chunks = float('inf')
-    channel_methods = []
+    schedule = {}
+    temp_priority = {}
     lock = Lock() 
 
     @jwt_required()
@@ -56,54 +57,81 @@ class ChannelsView(MethodView):
         jwt_token = auth_header.split("Bearer ")[1] if auth_header and auth_header.startswith("Bearer ") else None
         current_user_id = str(get_jwt_identity()[0])
         json_data = request.get_json()
-        channel_payload = json_data.get('channels')
         if request.path == '/settings':
             return self.handle_settings_post(current_user_id, json_data)
         else:
-            db_cursor.execute(f'select * from settings where UserId={current_user_id}')
-            result = db_cursor.fetchone()
-            if result:
-                with self.lock:
-                    self.threads = result[1]
-                    string_channels = result[2][1:-1]
-                    items = [item.strip() for item in string_channels.split(',')]
-                    string_priority = result[3][1:-1]
-                    items_priority = [item.strip() for item in string_priority.split(',')]
-                    self.priority = {items[i]:items_priority[i] for i in range(len(items)) if int(items_priority[i])!= 0}
-                db_cursor.execute(f"SELECT username FROM customers WHERE userid={current_user_id}")
-                user = db_cursor.fetchone()[0]
-                if user is not None:
-                    grouped_channels = {}
-                    for channel, priority in self.priority.items():
-                        if priority not in grouped_channels:
-                            grouped_channels[priority] = []
-                        grouped_channels[priority].append(channel)
+            channel_payload = json_data.get('channels')
+            channel_priority = json_data.get('channel_priority')
+            get_threads = json_data.get('threads')
+            proceed = bool(channel_priority==None or get_threads==None)
+            match proceed:
+                case True:
+                    db_cursor.execute(f'select * from settings where UserId={current_user_id}')
+                    result = db_cursor.fetchone()
+                    with self.lock:
+                        self.threads = result[1]
+                        self.temp_priority = result[2]
+                    self.temp_priority = json.loads(self.temp_priority)
+                    for processes in self.temp_priority:
+                        values = list(processes.values())
+                        self.priority[values[0]] = values[1]
+                        self.schedule[values[0]] = values[2]
+                case _:
+                    with self.lock:
+                        self.threads = get_threads
+                        self.temp_priority = channel_priority
+                    # self.temp_priority = json.loads(self.temp_priority)
+                    for processes in self.temp_priority:
+                        values = list(processes.values())
+                        self.priority[values[0]] = values[1]
+                        self.schedule[values[0]] = values[2]
+            
+            db_cursor.execute(f"SELECT username FROM customers WHERE userid={current_user_id}")
+            user = db_cursor.fetchone()[0]
+            if user is not None:
+                grouped_channels = {}
+                for channel, priority in self.priority.items():
+                    if priority not in grouped_channels:
+                        grouped_channels[priority] = []
+                    grouped_channels[priority].append(channel)
 
-                    execution_list = list(grouped_channels.values())
-                    with ThreadPoolExecutor(max_workers=self.threads) as executor:
-                        for group in execution_list:
-                            futures = []
-                            for channel in group:
-                                if channel in self.priority and hasattr(self, channel):
-                                    func = getattr(self, channel)
+                execution_list = list(grouped_channels.values())
+                with ThreadPoolExecutor(max_workers=self.threads) as executor:
+                    for group in execution_list:
+                        futures = []
+                        for channel in group:
+                            if channel in self.priority and hasattr(self, channel):
+                                func = getattr(self, channel)
+                                schedule = self.schedule[channel]
+                                if schedule == "0":
                                     futures.append(executor.submit(func, channel_payload[channel]))
-                            print(futures)
+                                else:
+                                    executor.submit(self.execute_with_schedule, func, channel_payload[channel], schedule)
+                        print(futures)
 
-                            wait(futures)
-                        return jsonify({"msg": "Execution complete"})
+                        wait(futures)
+                    return jsonify({"msg": "Execution complete"})
 
             else:
                 return jsonify({'msg': 'Please register your user settings'})
 
+
+    def execute_with_schedule(self, func, payload, schedule):
+        current_time = datetime.datetime.now()
+        start_hour, start_minute = map(int, schedule.split(':'))
+        scheduled_time = current_time.replace(hour=start_hour, minute=start_minute, second=0, microsecond=0)
+        
+        if current_time < scheduled_time:
+            time.sleep((scheduled_time - current_time).total_seconds())
+        
+        func(payload)
             
     def handle_settings_post(self, current_user_id, json_data):
         threads = json_data.get('threads')
-        channels = json_data.get('channels')
-        priority = json_data.get('priority')
-        channels_str = json.dumps(channels)
-        priority_str = json.dumps(priority)
+        channels_priority = json_data.get('channel_priority')
+        channels_str = json.dumps(channels_priority)
         try:
-            db_cursor.execute(f'INSERT INTO settings (UserId, threads, channels, priority) VALUES ({current_user_id}, {threads}, {channels_str}, {priority_str})')
+            db_cursor.execute(f"INSERT INTO settings (UserId, threads, channel_priority) VALUES ({current_user_id}, {threads}, '{channels_str}')")
             mydb.commit()
             return jsonify({'msg': 'Added record successfully'})
         except (ms.IntegrityError, ms.DataError) as err:
